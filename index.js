75965ce5c7284ed09923eff64491921441164d0f
DEWAN <dewanmahinchy@gmail.com>
2021-11-01 23:02:05 +0600

    port spelling correction

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
